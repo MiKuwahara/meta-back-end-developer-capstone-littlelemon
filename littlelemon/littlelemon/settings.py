b69c47ae8a0b1133b1e98bf486552a1bafa4c5da
Michelle Kuwahara <mxk200037@utdallas.edu>
2026-04-23 21:15:14 -0500

    Set up Booking and Menu APIs

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Own apps
+    
+    # Local
     'restaurant',
+    
+    # Third Party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework.renderers.JSONRenderer",
+    ]
+}
